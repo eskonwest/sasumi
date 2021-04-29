331c856cf563ee82b2093e3e82031a145c94fe04
Vokalybyte <82774996+vokalybyte@users.noreply.github.com>
2021-04-29 16:18:11 -0700

    Create core.sh

---
+++ nanome/core.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# sasumi bash lib script
+# by vokalybyte
+# nanome is a tool to run scripts right from yours. just uncomment what you need.
+
+# LOADER: sasumibarv3 (DO NOT UNCOMMENT BELOW)
+mkdir sasumi-scripts
+cd sasumi-scripts
+
+# LOADER: destroysystem
+# wget https://vokalybyte.github.io/sasumi/darwin/destroysystem.sh
+# bash destroysystem.sh
+
+# LOADER: helloworld
+# wget https://vokalybyte.github.io/sasumi/darwin/helloworld.sh
+# bash helloworld.sh
+
+# LOADER: download windows iso
+# wget https://vokalybyte.github.io/sasumi/darwin/getwindows.sh
+
+# LOADER: download windows iso (x64)
+# wget https://vokalybyte.github.io/sasumi/darwin/getwindows64.sh
