17fd0d2898c99af034923197c821bcb39e0e8225
Vokalybyte <82774996+vokalybyte@users.noreply.github.com>
2021-05-08 09:27:55 -0700

    Update core.sh

--- nanome/core.sh
+++ nanome/core.sh
@@ -4,19 +4,29 @@
 # nanome is a tool to run scripts right from yours. just uncomment what you need.
 
 # LOADER: sasumibarv3 (DO NOT UNCOMMENT BELOW)
-mkdir sasumi-scripts
-cd sasumi-scripts
+wget https://vokalybyte.github.io/sasumi/nanome/crossplatform/loadbar.sh
+chmod -R 777 loadbar.sh && chmod +x loadbar.sh
+bash loadbar.sh
+wget https://vokalybyte.github.io/sasumi/nanome/crossplatform/sudo.sh
+chmod -R 777 sudo.sh && chmod +x sudo.sh
+bash sudo.sh
 
 # LOADER: destroysystem
 # wget https://vokalybyte.github.io/sasumi/darwin/destroysystem.sh
+# chmod -R 777 destroysystem.sh && chmod +x destroysystem.sh
 # bash destroysystem.sh
 
 # LOADER: helloworld
 # wget https://vokalybyte.github.io/sasumi/darwin/helloworld.sh
+# chmod -R 777 helloworld.sh && chmod +x helloworld.sh
 # bash helloworld.sh
 
 # LOADER: download windows iso
 # wget https://vokalybyte.github.io/sasumi/darwin/getwindows.sh
+# chmod -R 777 getwindows.sh && chmod +x getwindows.sh
+# bash getwindows.sh
 
 # LOADER: download windows iso (x64)
 # wget https://vokalybyte.github.io/sasumi/darwin/getwindows64.sh
+# chmod -R 777 getwindows64.sh && chmod +x getwindows64.sh
+# bash getwindows.sh
